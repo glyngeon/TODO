e96995b594f99da793ebb709eae6e61eba30c3b3
gaurav <gauravgangwar767@gmail.com>
2022-07-24 20:44:50 +0530

    angular todo app with json-server

---
+++ src/app/components/add-todo/add-todo.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-add-todo',
+  templateUrl: './add-todo.component.html',
+  styleUrls: ['./add-todo.component.css']
+})
+export class AddTodoComponent implements OnInit {
+  task?:string;
+  @Output() addTodo : EventEmitter<any> = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }  
+
+  // adds the todo in the list
+  saveTask() {
+    const todo = 
+    {
+      title : this.task,
+      completed:false       
+    }
+    this.addTodo.emit(todo);
+    this.task = '';
+  }
+}
